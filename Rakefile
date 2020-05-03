b0e70339720933f26403a80a693d1b10623db30c
Vuong Phan <vuongphan139.it@gmail.com>
2020-05-03 15:35:59 +0700

    Chapter 1: Introduction

---
+++ Rakefile
@@ -0,0 +1,3 @@
+require File.expand_path('../config/application', __FILE__)
+
+MarketPlaceApi::Application.load_tasks
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarketPlaceApi::Application.config.secret_key_base = 'e135f57218c0709f92175154c457b98de1f4dff862c8fafdd1901268bb2d0f615fd4bd2bd829025a9b19018b81dd5b82367e106f532d82ba969308ec61e3b433'
